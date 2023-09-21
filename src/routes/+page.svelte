70837a6ba0b619e33c870fce29a22a1aaef0534e
Hoeun Lee <hoeunlee228@gmail.com>
2023-09-21 15:54:09 +0900

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    let count = 0;
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h3>Counter : {count}</h3>
+<button on:click={() => count++}>Increase</button>
+<button on:click={() => count--}>Decrease</button>
