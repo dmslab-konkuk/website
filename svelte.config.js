ef07b2852a1769dbba225694cfc6dc8f4147b768
Hoeun Lee <hoeunlee228@gmail.com>
2023-09-21 16:53:23 +0900

    test

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,8 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: dev ? '' : process.env.BASE_PATH,
+			// base: dev ? '' : process.env.BASE_PATH,
+			base: dev ? '' : '/leehe228.github.io',
 		}
 	}
 };
