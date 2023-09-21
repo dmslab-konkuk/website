0b7417134bc29a6d312540d7dc782ad75479df28
Hoeun Lee <hoeunlee228@gmail.com>
2023-09-22 01:58:37 +0900

    Update Post Page

--- src/routes/post/+page.js
+++ src/routes/post/+page.js
@@ -9,24 +9,8 @@ export async function load({ fetch }) {
         // @ts-ignore
         n = queryParams.get('n');
     }
-
-    let jdata = null;
-
-    try {
-        const response = await fetch('database/db.json'); 
-        if (response.ok) {
-            jdata = await response.json();
-            if ('posts' in jdata) {
-                if (jdata['posts'].length > parseInt(n) && parseInt(n) >= 0) {
-                    return jdata['posts'][n];
-                }
-            }
-        } else {
-            console.error('Failed to fetch JSON data');
-        }
-    } catch (error) {
-        console.error('Error fetching JSON data', error);
-    }
-
-    return JSON.parse("{\"title\": \"FAIL\"}");
+    
+    const response = await fetch('database/db.json'); 
+    const jdata = await response.json();
+    return jdata['posts'][n];
 }
--- src/routes/post/+page.svelte
+++ src/routes/post/+page.svelte
@@ -6,7 +6,18 @@
 <link rel="stylesheet" href="css/style.css">
 
 <div class="content">
-    <h1>{data.title}</h1>
+    <div class="t1">{data.title}</div>
+    <div class="spacer" style="height: 10px;"></div>
+    <div class="t2">{data.subtitle}</div>
+    <div class="spacer" style="height: 10px;"></div>
+    <div class="tag-block">
+        {#each typeof data.tags === 'undefined' ? [] : data.tags.split(',') as tag}
+            <!-- svelte-ignore a11y-missing-attribute -->
+            <div><a>#{tag}</a></div>
+        {/each}
+    </div>
+    <div class="spacer" style="height: 26px;"></div>
+    <div class="post-content">
 
-    <div></div>
+    </div>
 </div>
