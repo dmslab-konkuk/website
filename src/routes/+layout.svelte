57b797f794c86b8f93b2100b198ef7611da217c5
Hoeun Lee <hoeunlee228@gmail.com>
2023-09-21 17:32:06 +0900

    test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,13 @@
+<link rel="stylesheet" href="/css/style.css">
+
 <script>
     import { base } from '$app/paths';
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        console.log(document);
+    })
 </script>
-
-<h3>BASE : {base}</h3>
-
-<nav>
-    <a href="{base}/">Home</a>
-    <a href="{base}/post">Post</a>
-</nav>
 
 <slot>
 
