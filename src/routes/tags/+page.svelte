b5df28e2a6e86b8a58239bb81922fbae457efc90
Hoeun Lee <hoeunlee228@gmail.com>
2024-02-28 01:24:25 +0900

    add tags page

---
+++ src/routes/tags/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    // export let data;
+</script>
+
+<link rel="stylesheet" href="css/font.css" />
+<link rel="stylesheet" href="css/style.css" />
+
+<div class="content">
+    <div class="t1">태그</div>
+    <div class="spacer" style="height: 36px;"></div>
+    <p>태그 에 대한 검색 결과가 없습니다</p>
+</div>
