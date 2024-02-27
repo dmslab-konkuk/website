a3398b5f7368c420df000cdbc1b821c6829d922c
Hoeun Lee <hoeunlee228@gmail.com>
2024-02-27 11:04:35 +0900

    update pages

--- src/routes/post/+page.js
+++ src/routes/post/+page.js
@@ -4,7 +4,7 @@ import { error } from '@sveltejs/kit';
 
 export async function load({ fetch }) {
 
-    let n = '0';
+    let n = '-1';
     if (typeof window !== 'undefined') {
         const currentURL = new URL(window.location.href);
         const queryParams = currentURL.searchParams;
