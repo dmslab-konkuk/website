4bd1010bc4c85072de88355a2899aa5c4190b6fa
Hoeun Lee <hoeunlee228@gmail.com>
2024-03-11 19:58:30 +0900

    update publication page

---
+++ src/routes/publication/+page.svelte
@@ -0,0 +1,240 @@
+<div class="content">
+    <div class="spacer" style="height: 50px;"></div>
+    <div class="t1">Publication</div>
+    <div class="spacer" style="height: 20px;"></div>
+    <div class="t2">Journal/Conference Papers</div>
+    <div class="spacer" style="height: 12px;"></div>
+    <div class="paper-content">
+        <li>
+            Kaliappan, V. K., Yu, S., Soundararajan, R., Jeon, S., Min, D., &
+            Choi, E. (2022). <b
+                >High-Secured Data Communication for Cloud Enabled Secure Docker
+                Image Sharing Technique Using Blockchain-Based Homomorphic
+                Encryption.</b
+            > Energies, 15(15), 5544. https://doi.org/10.3390/en15155544
+        </li>
+        <li>
+            Battisti, F., Silva, A., Pereira, L., Carvalho, T., Araujo, J.,
+            Choi, E., … & Min, D. (2022). hLSTM-Aging: A Hybrid LSTM Model for
+            Software Aging Forecast. Applied Sciences, 12(13), 6412.
+            https://doi.org/10.3390/app12136412
+        </li>
+        <li>
+            Silva, F. A., Fé, I., Brito, C., Araújo, G., Feitosa, L., Choi, E.,
+            … & Nguyen, T. A. (2022). Supporting availability evaluation of a
+            smart building monitoring system aided by fog computing. Electronics
+            Letters, 58(12), 471-473. https://doi.org/10.1049/ell2.12502
+        </li>
+        <li>
+            Fé, I., Matos, R., Dantas, J., Melo, C., Nguyen, T. A., Min, D., … &
+            Maciel, P. R. M. (2022). Performance-Cost Trade-Off in Auto-Scaling
+            Mechanisms for Cloud Computing. Sensors, 22(3), 1221.
+            https://doi.org/10.3390/s22031221
+        </li>
+        <li>
+            Kaliappan, V. K., Nguyen, T. A., Min, D., Lee, J. W., & Sakthi, U.
+            (2022). Steering Behavior-based Multiple RUAV Obstacle Avoidance
+            Control. INTELLIGENT AUTOMATION AND SOFT COMPUTING, 34(1), 575-591.
+            https://doi.org/10.32604/iasc.2022.024577
+        </li>
+        <li>
+            Nguyen, T. A., Kim, M., Lee, J., Min, D., Lee, J. W., & Kim, D.
+            (2022). Performability evaluation of switch-over Moving Target
+            Defence mechanisms in a Software Defined Networking using stochastic
+            reward nets. Journal of Network and Computer Applications, 199,
+            103267. https://doi.org/10.1016/j.jnca.2021.103267
+        </li>
+        <li>
+            Victor, C., Nguyen, T. A., Silva, L. A., Andrade, E., Santos, G. L.,
+            Min, D., … & Silva, F. A. (2021, September). Performability
+            assessment and sensitivity analysis of a home automation system. In
+            2021 IEEE/ACM 25th International Symposium on Distributed Simulation
+            and Real Time Applications (DS-RT) (pp. 1-4). IEEE.
+            https://doi.org/10.1109/DS-RT52167.2021.9576142 (SCIE,
+            JCR.Q2.Engineering, 77/266, IF2022=3.9)
+        </li>
+        <li>
+            NGUYEN, Tuan Anh, et al. Performability evaluation of load balancing
+            and fail-over strategies for medical information systems with
+            edge/fog computing using stochastic reward nets. Sensors, 2021,
+            21.18: 6253. https://doi.org/10.3390/s21186253 (SCIE,
+            JCR.Q2.Engineering, 77/266, IF2022=3.9)
+        </li>
+        <li>
+            Brito, C., Silva, L., Callou, G., T. A. Nguyen, Min, D., Lee, J.-W.,
+            & Silva, F. A. (2021). Offloading Data through Unmanned Aerial
+            Vehicles: A Dependability Evaluation. Electronics, 10(16), 1916.
+            https://doi.org/10.3390/electronics10161916 (SCIE, JCR.Q3.CS.IS,
+            93/162, IF2020=2.397)
+        </li>
+        <li>
+            T. A. Nguyen, D. Min, E. Choi, and J.-W. Lee, “Dependability and
+            Security Quantification of an Internet of Medical Things
+            Infrastructure based on Cloud-Fog-Edge Continuum for Healthcare
+            Monitoring using Hierarchical Models,” IEEE Internet of Things J.,
+            pp. 1–1, 2021, doi: 10.1109/JIOT.2021.3081420.(SCIE, JCR.Q1.CS.IS,
+            Ranking 3/156, IF2019=9.936)
+        </li>
+        <li>
+            Brito, C.; Rodrigues, L.; Santos, B.; Fé, I.; Nguyen, T.A.; Min, D.;
+            Lee, J.-W.; Silva, F.A. Stochastic Model Driven Performance and
+            Availability Planning for a Mobile Edge Computing System. Appl. Sci.
+            2021, 11, 4088. doi: 10.3390/app11094088 (SCIE, JCR.Q2.Eng, Ranking
+            32/91, IF2019=2.474)
+        </li>
+        <li>
+            F. A. Silva, T. A. Nguyen, I. Fe, C. Brito, D. Min, and J.-W. Lee,
+            “Performance Evaluation of an Internet of Healthcare Things for
+            Medical Monitoring Using M/M/c/K Queuing Models,” IEEE Access, vol.
+            9, pp. 55271–55283, 2021, doi: 10.1109/ACCESS.2021.3071508. (SCIE,
+            JCR.Q1.CS.IS, Ranking 35/156, IF2019=3.745)
+        </li>
+        <li>
+            I. Gonçalves, L. Rodrigues, F. A. Silva, T. A. Nguyen, D. Min, and
+            J.-W. Lee, “Surveillance System in Smart Cities: A Dependability
+            Evaluation Based on Stochastic Models,” Electronics, vol. 10, no. 8,
+            p. 876, Apr. 2021, doi: 10.3390/electronics10080876. (SCIE,
+            JCR.Q2.Eng&E.E, Ranking 154/266, IF2019=2.412)
+        </li>
+        <li>
+            T. A. Nguyen, D. Min, and E. Choi, “A Hierarchical Modeling and
+            Analysis Framework for Availability and Security Quantification of
+            IoT Infrastructures,” Electronics, vol. 9, no. 1, p. 155, Jan. 2020.
+            DOI: 10.3390/electronics9010155 (SCIE, JCR.Q2.Eng&E.E, Ranking
+            154/266, IF2019=2.412)
+        </li>
+        <li>
+            T. A. Nguyen, D. Min, E. Choi, and T. D. Thang, “Reliability and
+            Availability Evaluation for Cloud Data Center Networks using
+            Hierarchical Models​,” IEEE Access, Jan. 2019. DOI:
+            10.1109/ACCESS.2019.2891282 (SCIE, JCR.Q1.CS.IS, Ranking 23/155,
+            IF2018=4.098)
+        </li>
+        <li>
+            T. A. Nguyen, K. Han, D. Min, E. Choi, T. D. Thang, and Y.-J. Choi,
+            “A stochastic reward net-based assessment of reliability,
+            availability and operational cost for a software-defined network
+            infrastructure,” J. Supercomput., Nov. 2018. DOI:
+            10.1007/s11227-018-2677-y (SCI/SCIE, JCR.Q2.CS.Theory&Methods,
+            Ranking 52/104, IF2017=1.532, IF2018=2.157)
+        </li>
+        <li>
+            T. A. Nguyen, D. Min, and E. Choi, “A comprehensive evaluation of
+            availability and operational cost for a virtualized server system
+            using stochastic reward nets,” J. Supercomput., pp. 1–55, Aug. 2017,
+            DOI: 10.1007/s11227-017-2127-2, (SCI/SCIE, JCR.Q2.CS.Theory&Methods,
+            Ranking 52/104, IF2016=1.326, IF2018=2.157)
+        </li>
+        <li>
+            T. A. Nguyen, D. Min, and J. S. Park, “A Comprehensive Sensitivity
+            Analysis of a Data Center Network with Server Virtualization for
+            Business Continuity,” Math. Probl. Eng., vol. 2015, pp. 1–20, 2015,
+            DOI: 10.1155/2015/521289, (SCIE, JCR.Q3.Eng, Ranking 59/85, IF2015 =
+            0.76, IF2018 = 1.179)
+        </li>
+        <li>
+            Nguyen, T. A., Min, D. & Choi, E. Stochastic Reward Net-based
+            Modeling Approach for Availability Quantification of Data Center
+            Systems. in Dependability Engineering (ed. García Márquez, F. P.)
+            (InTech, 2018). DOI: 10.5772/intechopen.74306
+        </li>
+        <li>
+            Nguyen, T.A., Min, D.K., Lee, J.W., (2020), “iUAM: Service
+            Survivability of Digital Twin Urban Aerial Mobility”, The Korean
+            Society for Aeronautical & Space Sciences, 한국항공우주학회
+            학술발표회 초록집, 2020.11, 689-690. (@DBpia)
+        </li>
+        <li>
+            T. A. Nguyen, D. Min, and E. Choi, “Largeness Problem Mitigation for
+            Stochastic Reward Nets in Modeling and Analysis of a large IoT
+            system,” in The 6th International Conference on Big Data
+            Applications and Services (BIGDAS 2018), August 19-22, 2018,
+            TusStar, Zhengzhou, Henan, China.
+        </li>
+        <li>
+            L. Zou, X. Rui, T. A. Nguyen, D. Min, E. Choi, D. T. Tran, N. S.
+            Nguyen, “A Scalable Network Area Storage with Virtualization:
+            Modeling and Evaluation using Stochastic Reward Nets”, in
+            proceedings of 2018 International Conference on Information Science
+            and System (ICISS ‘18), April 27–29, 2018, Jeju, Republic of Korea,
+            ACM ISBN 978-1-4503-6421-8/18/04, DOI: 10.1145/3209914.3209918
+        </li>
+        <li>
+            T.A. Nguyen, D. Min, E. Choi, “Reliability and Availability Modeling
+            of Fog Computing based IoT System”, in The 5th International
+            Conference on Big Data Applications and Services, BIGDAS 2017,
+            November 23-25, 2017, Jeju Island, South Korea
+        </li>
+        <li>
+            T. A. Nguyen, X. Rui, D. Lim, J. Oh, D. Min, E. Choi, D. T. Tran,
+            and Nhu Son Nguyen, “Model-based sensitivity of a disaster tolerant
+            active-active genesis cloud system,” in proceedings of 3rd EAI
+            International Conference on Industrial Networks and Intelligent
+            Systems, INISCOM2017, Sep. 04, 2017, Ho Chi Minh city, Vietnam,
+            online published in: Chen Y., Duong T. (eds) Industrial Networks and
+            Intelligent Systems. INISCOM 2017. Lecture Notes of the Institute
+            for Computer Sciences, Social Informatics and Telecommunications
+            Engineering, vol 221. Springer, Cham, DOI:
+            10.1007/978-3-319-74176-5_20.
+        </li>
+        <li>
+            K. Han, T. A. Nguyen, D. Min, and E. M. Choi, “An Evaluation of
+            Availability, Reliability and Power Consumption for a SDN
+            Infrastructure using Stochastic Reward Net,” Lect. Notes Electr.
+            Eng. - Springer, 2016, DOI: 10.1007/978-981-10-3023-9_98
+        </li>
+        <li>
+            A hierarchical modeling and analysis framework for availability and
+            security quantification of IoT infrastructures, “Nguyen TA, Min D,
+            Choi E.”, Electron, 2020;9(1):1-30.
+            https://doi.org/10.3390/electronics9010155
+        </li>
+        <li>
+            A stochastic reward net-based assessment of reliability,
+            availability and operational cost for a software-defined network
+            infrastructure, “Nguyen TA, Han K, Min D, Choi E, Thang TD, Choi
+            YJ.”, J Supercomput, 2019;75(8):4657-4683.
+            https://doi.org/10.1007/s11227-018-2677-y
+        </li>
+        <li>
+            Reliability and availability evaluation for cloud data center
+            networks using hierarchical models, “Nguyen TA, Min D, Choi E, Tran
+            TD.”, IEEE Access, 2019;7:9273-9313.
+            https://doi.org/10.1109/ACCESS.2019.2891282
+        </li>
+        <li>
+            Model-based sensitivity of a disaster tolerant active-active GENESIS
+            cloud system, “Nguyen TA, Rui X, Lim D, et al.”, Lect Notes Inst
+            Comput Sci Soc Telecommun Eng LNICST, 2018;221:228-241.
+            https://doi.org/10.1007/978-3-319-74176-5_20
+        </li>
+        <li>
+            A scalable network area storage with virtualization: Modelling and
+            evaluation using stochastic reward nets, “Zou L, Rui X, Anh Nguyen
+            T, et al.”, ACM Int Conf Proceeding Ser, Published online
+            2018:225-233. https://doi.org/10.1145/3209914.3209918
+        </li>
+        <li>
+            A Comprehensive Evaluation of Availability and Operational Cost for
+            a Virtualized Server System Using Stochastic Reward Nets, “Nguyen
+            TA, Min D, Choi E.”, Vol 74. Springer US; 2018.
+            https://doi.org/10.1007/s11227-017-2127-2
+        </li>
+        <li>
+            Reliability and Availability Modeling of Fog Computing based IoT
+            System, “Nguyen TA, Min D, Choi E.”, 5th Int Conf Big Data Appl Serv
+            (BIGDAS 2017), Published online 2017.
+        </li>
+        <li>
+            Efficient Spatial-Temporal RDF Data Processing in Distributed
+            Semantic Web Environment, “Shin I, Min D.”, J Inf Technol Archit,
+            2017;14(4):367-373.
+        </li>
+        <li>
+            HIM-PRS: A patent recommendation system based on hierarchical
+            index-based mapreduce framework, “Rui X, Min D.”, Adv Comput Sci
+            Ubiquitous Comput Lect Notes Electr Eng, 2017;421:843-848.
+            https://doi.org/10.1007/978-981-10-3023-9_130
+        </li>
+    </div>
+</div>
